c7935b69f5e1768015ceeb024505e0a98307085f
rie <kou.kotetsu@gmail.com>
2020-08-15 15:42:21 +0900

    [Add]

---
+++ app/controllers/books_controller.rb
@@ -0,0 +1,54 @@
+class BooksController < ApplicationController
+  before_action :authenticate_user!
+
+  def show
+    @book_find = Book.find(params[:id])
+    @book = Book.new(book_params)
+    @user = @book_find.user
+  end
+
+  def index
+    @books = Book.all
+    @book = Book.new(book_params)
+    @current_user = current_user
+  end
+
+  def create
+    @book = Book.new(book_params)
+    @book.user_id = current_user.id
+    if @book.save
+      redirect_to book_path(@book), notice: "You have created book successfully."
+    else
+      @books = Book.all
+      @current_user = current_user
+      render 'index'
+    end
+  end
+
+  def edit
+    @book = Book.find(params[:id])
+  end
+
+  def update
+    @book = Book.find(params[:id])
+    if @book.update(book_params)
+      redirect_to book_path(@book), notice: "You have updated book successfully."
+    else
+
+      render "edit"
+    end
+  end
+
+  def destroy
+    @book = Book.find(params[:id])
+    @book.destoy
+    redirect_to books_path
+  end
+
+  private
+
+  def book_params
+    params.require(:book).permit(:title, :body)
+  end
+
+end
